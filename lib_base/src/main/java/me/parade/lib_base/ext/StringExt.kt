406171ee84daba436245a7d0800d5bbb6e573441
flb <13703937803@163.com>
2024-09-13 14:13:51 +0800

    添加字符串的几个扩展函数

--- lib_base/src/main/java/me/parade/lib_base/ext/StringExt.kt
+++ lib_base/src/main/java/me/parade/lib_base/ext/StringExt.kt
@@ -4,6 +4,10 @@ import android.graphics.Bitmap
 import android.os.Build
 import android.webkit.MimeTypeMap
 import com.blankj.utilcode.util.FileUtils
+import com.google.gson.JsonElement
+import com.google.gson.JsonParser
+import java.security.MessageDigest
+import java.util.regex.Pattern
 
 /**
  * 根据URL路径获取文件名
@@ -38,6 +42,9 @@ fun String.getMimeTypeFromFromUrl(defaultType:String = "image/*"):String{
     return MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension)?:defaultType
 }
 
+/**
+ * 获取BitMap.CompressFormat
+ */
  fun String.getBitmapFormat(): Bitmap.CompressFormat {
     val fileName = this.lowercase()
     return when {
@@ -48,3 +55,89 @@ fun String.getMimeTypeFromFromUrl(defaultType:String = "image/*"):String{
         else -> Bitmap.CompressFormat.PNG
     }
 }
+
+/**
+ * 实现手机号中间4位显示*或者切换不显示*
+ * @param originalPhone 真实手机号
+ */
+fun String.handlePhoneMiddleText(originalPhone: String): String {
+    return if (contains("*")) originalPhone else this.replace(
+        Regex("(\\d{3})\\d{4}(\\d{4})"),
+        "$1****$2"
+    )
+
+}
+
+/**
+ * 从url中获取指定参数值
+ */
+fun String.getParamByUrl(name:String):String?{
+
+    var url = this
+    url += "&"
+    val regex = "([?&])#?${name}=[a-zA-Z0-9]*(&)?"
+    val pattern = Pattern.compile(regex)
+    val matcher = pattern.matcher(url)
+    return if (matcher.find()) {
+        matcher.group(0)!!.split("=")[1].replace("&","")
+    } else null
+}
+
+/**
+ * 字符串MD5加密
+ */
+fun String.getMd5String(): String {
+    val instance = MessageDigest.getInstance("md5").also { it.update(this.toByteArray()) }
+    return instance.digest().joinToString(separator = "") { "%02x".format(it) }
+}
+
+/**
+ * 一个字符串里某个特定的字符串出现的次数
+ * @param letter 要匹配的字符串
+ */
+fun String.timesOfStr(letter: String):Int{
+    val pattern = Pattern.compile(letter)
+    val matcher = pattern.matcher(this)
+    var times = 0
+    while (matcher.find()){
+        times++
+    }
+    return times
+}
+
+/**
+ * 查找指定字符在字符串第n次出现的位置
+ * @param letter 指定的字符
+ * @param position 第几次出现
+ * @return 出现的位置 找不到返回-1
+ */
+fun String.findIndexOfPosition(letter: String, position: Int): Int {
+    val pattern = Pattern.compile(letter)
+    val matcher = pattern.matcher(this)
+    var num = 0
+    while (matcher.find()) {
+        num++
+        if (num == position) break
+    }
+    return try {
+        matcher.start()
+    } catch (e: IllegalStateException) {
+        -1
+    }
+}
+
+/** 是否是合法的json字符串 */
+fun String.isValidateJson(): Boolean {
+    val jsonElement: JsonElement?
+    try {
+        jsonElement = JsonParser().parse(this)
+    } catch (e: Exception) {
+        return false
+    }
+
+    if (jsonElement == null) {
+        return false
+    }
+    return jsonElement.isJsonArray || jsonElement.isJsonObject || jsonElement.isJsonPrimitive
+}
+
