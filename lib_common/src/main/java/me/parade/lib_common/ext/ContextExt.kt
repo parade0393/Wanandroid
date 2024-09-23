b33f4a698ccc987f7af3174b5ff294865c131168
flb <13703937803@163.com>
2024-09-23 17:52:35 +0800

    gps是否打开

--- lib_common/src/main/java/me/parade/lib_common/ext/ContextExt.kt
+++ lib_common/src/main/java/me/parade/lib_common/ext/ContextExt.kt
@@ -1,5 +1,12 @@
 package me.parade.lib_common.ext
 
+import android.content.Context
+import android.location.LocationManager
+
+fun Context.checkGPSIsOpen():Boolean{
+    val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+    return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+}
 
 
 
