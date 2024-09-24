730c015901c9925b8c6d70388ed1cc391de61e18
flb <13703937803@163.com>
2024-09-24 15:57:55 +0800

    添加工具类和包可见性配置

--- lib_common/src/main/java/me/parade/lib_common/ext/ContextExt.kt
+++ lib_common/src/main/java/me/parade/lib_common/ext/ContextExt.kt
@@ -8,5 +8,28 @@ fun Context.checkGPSIsOpen():Boolean{
     return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
 }
 
+/**
+ * 是否安装了某个应用
+ */
+fun Context.isInstallAvailable(packageName: String):Boolean{
+    val installedPackages = packageManager.getInstalledPackages(0)// 获取所有已安装程序的包信息
+    installedPackages.forEach {
+        if (packageName == it.packageName){
+            return true
+        }
+    }
+    return false
+}
 
+//是否安装了微信
+fun Context.isWeixinAvilible():Boolean{
+   return isInstallAvailable("com.tencent.mm")
+}
+
+/**
+ * 是否安装了支付宝
+ */
+fun Context.isAliPayInstalled():Boolean{
+    return isInstallAvailable("com.eg.android.AlipayGphone")
+}
 
